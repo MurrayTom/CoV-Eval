8fe813808fd1528314db0c9fa7272290a8f7753b
MurrayTom <163309363+MurrayTom@users.noreply.github.com>
2025-07-23 20:26:41 +0800

    Delete github.sh

--- github.sh
+++
@@ -1,7 +0,0 @@
-git init
-git config --global user.email "MurrayTom1998@gmail.com" 
-git config --global user.name "MurrayTom" 
-git add . 
-git commit -m "first commit"
-git remote add origin https://github.com/MurrayTom/CoV-Eval.git
-git push -u origin master
